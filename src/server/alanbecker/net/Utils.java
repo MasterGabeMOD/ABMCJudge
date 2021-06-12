b168b1ca2531801ee8d42e6478cda4af0f19be60
MasterGabeMOD <MasterGabeMod@yahoo.com>
2021-06-12 16:21:25 -0700

    Beginning of judges

---
+++ src/server/alanbecker/net/Utils.java
@@ -0,0 +1,12 @@
+package server.alanbecker.net;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+
+	public static String chat (String s) {
+		return ChatColor.translateAlternateColorCodes('&', s);
+		
+	}
+	
+}
--- src/server/alanbecker/net/main.java
+++ src/server/alanbecker/net/main.java
@@ -2,4 +2,10 @@ package server.alanbecker.net;
 
 public class main {
 
+
+public void onEnable(){
+	
+	
+	
+}
 }
